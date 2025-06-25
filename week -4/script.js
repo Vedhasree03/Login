65ef7243b544c63ee652eb01380da7029740d447
Vedhasree03 <gudevedhasree@gmail.com>
2025-06-25 22:04:10 +0530

    Add files via upload

---
+++ week -4/script.js
@@ -0,0 +1,16 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (!name || !email || !message) {
+    alert("Please fill in all fields.");
+    e.preventDefault();
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email.");
+    e.preventDefault();
+  }
+});
